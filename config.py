08b9f27ed31e37dc42441f4eb4c0e13d91ef748d
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2025-11-17 15:47:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = ("29245477")
 API_HASH = ("0abc83883262245c90ca337b7a0375c4")
-BOT_TOKEN = ("7845096754:AAG9ll3MheYR4tOOLbBq7pnzrufPH-_KkLA")
+BOT_TOKEN = ("")
 
 # This is optional if you want a fallback RTMP URL
 DEFAULT_RTMP_URL = ("rtmps://dc5-1.rtmp.t.me/s/")
